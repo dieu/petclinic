d958955963ab83529c21e5aec5007d2aacd26fdb
Dmytro Makhno <dmakhno@griddynamics.com>
2013-03-28 14:14:23 +0200

    fix for overwrite same results

--- report/collect.sh
+++ report/collect.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 JUNIT_TEST_REPORTS=$1
 FILE_NAME=$2
-(cat header.txt;echo "<root>";tail -n+2 -q $JUNIT_TEST_REPORTS/*.xml;echo "</root>") >> $2.xml
+(cat header.txt;echo "<root>";tail -n+2 -q $JUNIT_TEST_REPORTS/*.xml;echo "</root>") > $2.xml
